74e2d57162754182cec705e721f059aa9fcf795f
Dylan Sieren <dylan.sieren01@gmail.com>
2018-12-29 00:17:42 -0600

    finished with home page currently. added in a testimonial slider

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+const previousButton = document.querySelector(".previous");
+const nextButton = document.querySelector(".next")
+
+previousButton.addEventListener("click", () => moveContent(-1) );
+nextButton.addEventListener("click", () => moveContent(1) );
+
+showContent(slideIndex);
+
+function moveContent(slideIndexValue) {
+  showContent(slideIndex += slideIndexValue);
+}
+function showContent(slideIndexValue) {
+  const slideContainer = document.getElementsByClassName("slide-container");
+  if(slideIndexValue > slideContainer.length) {
+    slideIndex = 1;
+  }
+  if(slideIndexValue < 1) { 
+    slideIndex = slideContainer.length
+  }
+    for(let i = 0; i < slideContainer.length; i++) {
+      slideContainer[i].style.display = "none";
+    }
+    slideContainer[slideIndex-1].style.display = "flex";
+  }  
