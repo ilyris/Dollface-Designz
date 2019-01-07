896c26abe199bf3fd0cad5b8173b67b9d633abfc
Dylan Sieren <dylan.sieren01@gmail.com>
2019-01-07 02:16:50 -0600

    started the slide out navigation menu

--- scripts.js
+++ scripts.js
@@ -4,23 +4,52 @@ let slideIndex = 1;
 const previousButton = document.querySelector(".previous");
 const nextButton = document.querySelector(".next");
 
+
 previousButton.addEventListener("click", () => moveContent(-1));
 nextButton.addEventListener("click", () => moveContent(1));
+document.querySelector(".hamburger-menu").addEventListener("click", slideMenuOut);
 showContent(slideIndex);
 
 window.onscroll = () => {
   toggleScrollTopButton();
 };
-// window.onresize = () => {
-//   toggleMobileMenu();
-// };
-// window.onload = () => {
-//   toggleMobileMenu();
+
+// function toggleMoreMenu() {
+//   const mainMenu = document.querySelector(".main-navigation");
+//   this.classList.toggle("toggle-active");
+//   mainMenu.classList.toggle("on");
 // }
 
-function toggleMenu() {
+//Maybe break this big function into smaller functions
+function slideMenuOut() {
+  const mainMenuSlideOut = document.querySelector(".main-navigation-container");
+  const mainMenuUl = document.querySelector(".main-navigation");
+  const mainMenuLi = document.getElementsByClassName("main-navigation-li");
+  const mainMenuA = document.getElementsByClassName("main-navigation-a");
+  
+
   this.classList.toggle("toggle-active");
-  document.querySelector(".main-ul").classList.toggle("on");
+  mainMenuSlideOut.classList.toggle("on");
+  mainMenuUl.classList.toggle("mobile-active-ul");
+  for(value of mainMenuLi) {
+  value.classList.toggle("mobile-active-li");
+  }
+  for(value of mainMenuA) {
+    value.classList.toggle("mobile-active-a");
+    }
+  // if(mainMenuSlideOut.classList.contains('on') == true) {
+  //   for ( let i = 0; i < menuLinksLi.length; i++) {
+  //       menuLinksLi[i].classList.add("fadeInRightBig");
+  //       menuLinksLi[i].classList.remove('fadeOutRightBig');
+
+  //     }
+  // }
+  // if (mainMenuSlideOut.classList.contains("on") == false) {
+  //   for (let i = 0; i < menuLinksLi.length; i++) {
+  //       menuLinksLi[i].classList.add("fadeOutRightBig");
+  //       menuLinksLi[i].classList.remove('fadeInRightBig');
+  //     }
+  // }
 }
 
 // Toggle the class active on the scroll to top button, so after a certain scroll height it appears and disappears
