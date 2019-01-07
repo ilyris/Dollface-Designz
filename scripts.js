8362283492486c107f3f14155d26fb4803775b05
Dylan Sieren <dylan.sieren01@gmail.com>
2019-01-06 20:39:04 -0600

    added the top scroll button functionality, it only appears after 999 scroll height, and is removed under 999

--- scripts.js
+++ scripts.js
@@ -1,25 +1,41 @@
+// will need to animate elements, have the scroll to button fly in / out on certain heights. parallax the home page image with a mouse over preserve 3d effect
+//
+
 let slideIndex = 1;
 const previousButton = document.querySelector(".previous");
-const nextButton = document.querySelector(".next")
+const nextButton = document.querySelector(".next");
 
-previousButton.addEventListener("click", () => moveContent(-1) );
-nextButton.addEventListener("click", () => moveContent(1) );
-
+previousButton.addEventListener("click", () => moveContent(-1));
+nextButton.addEventListener("click", () => moveContent(1));
 showContent(slideIndex);
 
+window.onscroll = () => {
+  toggleScrollTopButton();
+};
+
+// Toggle the class active on the scroll to top button, so after a certain scroll height it appears and disappears
+const toggleScrollTopButton = () => {
+  const topScrollButton = document.querySelector(".top-scroll-container");
+  let scrollPosition = window.scrollY;
+  if (scrollPosition <= 999) {
+    topScrollButton.classList.remove("active");
+  } else if (scrollPosition >= 1000) {
+    topScrollButton.classList.add("active");
+  }
+};
 function moveContent(slideIndexValue) {
-  showContent(slideIndex += slideIndexValue);
+  showContent((slideIndex += slideIndexValue));
 }
 function showContent(slideIndexValue) {
   const slideContainer = document.getElementsByClassName("slide-container");
-  if(slideIndexValue > slideContainer.length) {
+  if (slideIndexValue > slideContainer.length) {
     slideIndex = 1;
   }
-  if(slideIndexValue < 1) { 
-    slideIndex = slideContainer.length
+  if (slideIndexValue < 1) {
+    slideIndex = slideContainer.length;
   }
-    for(let i = 0; i < slideContainer.length; i++) {
-      slideContainer[i].style.display = "none";
-    }
-    slideContainer[slideIndex-1].style.display = "flex";
-  }  
+  for (let i = 0; i < slideContainer.length; i++) {
+    slideContainer[i].style.display = "none";
+  }
+  slideContainer[slideIndex - 1].style.display = "flex";
+}
