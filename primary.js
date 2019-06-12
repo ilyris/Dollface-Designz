408f55413aeb4827297c7a8abc6c29d26d8d8c1d
Dylan Sieren <dylan.sieren01@gmail.com>
2019-06-11 23:18:12 -0500

    cleaned up the functions by appending classList at the end of my variables so we can just set up the variable name then the function almost like its jquery also started to create a class to create an object of my functions and my events for the mobileMenu still trying to debug why the hamburger event listener wont work

--- primary.js
+++ primary.js
@@ -5,9 +5,8 @@ $(document).ready(function(){
 });
 ///////////////////////////
 const mobileMenuCloseButton = document.getElementById('closeMobileMenuButton');
-
-document.querySelector(".hamburger-menu").addEventListener("click", toggleMenu);
 mobileMenuCloseButton.addEventListener('click', closeMobileMenu, false);
+document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu, false);
 
 
 // Event Listeners
@@ -43,45 +42,77 @@ function toggleStickyNavbar() {
 
 function toggleMenu() {
   this.classList.toggle("toggle-active");
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
-  const mobileMenuWrapper = document.getElementById('mobieMenuWrapper');
-  const mobileMenuOverlay = document.getElementById('mobileMenuOverlay');
+  const hamburgerMenu = document.querySelector('.hamburger-menu').classList;
+  const wrapper = document.getElementById('mobileMenuWrapper').classList;
+  const overlay = document.getElementById('mobileMenuOverlay').classList;
+  const mobileMenuCloseButton = document.getElementById('closeMobileMenuButton').classList;
 
 
-
-
-  if(hamburgerMenu.classList.contains("toggle-active")) {
-    mobileMenuCloseButton.classList.add('on');
-    mobileMenuWrapper.classList.add("mobileOn");
-    mobileMenuWrapper.classList.toggle("fadeInLeft");
-    mobileMenuOverlay.classList.add('fadeIn');
-    mobileMenuOverlay.classList.remove('fadeOut');
-      if(mobileMenuWrapper.classList.contains('fadeOutLeft')) {
-        mobileMenuWrapper.classList.remove("fadeOutLeft");
+  if(hamburgerMenu.contains("toggle-active")) {
+    mobileMenuCloseButton.add('on');
+    wrapper.add("mobileOn");
+    wrapper.toggle("fadeInLeft");
+    overlay.add('fadeIn');
+    overlay.remove('fadeOut');
+      if(wrapper.contains('fadeOutLeft')) {
+        wrapper.remove("fadeOutLeft");
       }
-  } else if(!hamburgerMenu.classList.contains('toggle-active')) {
-    mobileMenuWrapper.classList.remove("fadeInLeft");
-    mobileMenuWrapper.classList.toggle("fadeOutLeft");
-    mobileMenuOverlay.classList.remove('fadeIn');
-    mobileMenuOverlay.classList.add('fadeOut');
-    mobileMenuCloseButton.classList.remove('on');
+
+  } else if(!hamburgerMenu.contains('toggle-active')) {
+    wrapper.remove("fadeInLeft");
+    wrapper.toggle("fadeOutLeft");
+    overlay.remove('fadeIn');
+    overlay.add('fadeOut');
+    mobileMenuCloseButton.remove('on');
   }
 
 }
 
 function closeMobileMenu() {
-  const mobileMenuWrapper = document.getElementById('mobieMenuWrapper');
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
-  const mobileMenuOverlay = document.getElementById('mobileMenuOverlay');
+  const hamburgerMenu = document.querySelector('.hamburger-menu').classList;
+  const wrapper = document.getElementById('mobileMenuWrapper').classList;
+  const overlay = document.getElementById('mobileMenuOverlay').classList;
+  const mobileMenuCloseButton = document.getElementById('closeMobileMenuButton').classList;
 
-  if(mobileMenuWrapper.classList.contains('fadeInLeft')) {
-    hamburgerMenu.classList.remove('toggle-active');
-    mobileMenuWrapper.classList.remove('fadeInLeft');
-    mobileMenuWrapper.classList.add('fadeOutLeft');
-    mobileMenuOverlay.classList.remove('fadeIn');
-    mobileMenuOverlay.classList.add('fadeOut');
+  if(wrapper.contains('fadeInLeft')) {
+    hamburgerMenu.remove('toggle-active');
+    wrapper.remove('fadeInLeft');
+    wrapper.add('fadeOutLeft');
+    overlay.remove('fadeIn');
+    overlay.add('fadeOut');
   }
-  mobileMenuCloseButton.classList.remove('on');
+  mobileMenuCloseButton.remove('on');
 }
 
 
+// class MobileMenu {
+//   constructor() {
+//     this.hamburgerMenu = document.querySelector('.hamburger-menu');
+//     this.wrapper = document.getElementById('mobileMenuWrapper').classList;
+//     this.overlay = document.getElementById('mobileMenuOverlay').classList;
+//     this.events()
+//   }
+//   events() {
+//     this.hamburgerMenu.classList.addEventListener('click', this.toggle.bind(this));
+//   }
+//     toggleMenu() {
+//     this.toggle("toggle-active");
+//     if(hamburgerMenu.classList.contains("toggle-active")) {
+//       this.mobileMenuCloseButton.add('on');
+//       this.wrapper.add("mobileOn");
+//       this.wrapper.toggle("fadeInLeft");
+//       this.overlay.add('fadeIn');
+//       this.overlay.remove('fadeOut');
+//         if(this.wrapper.contains('fadeOutLeft')) {
+//           this.wrapper.remove("fadeOutLeft");
+//         }
+//     } else {
+//       this.wrapper.remove("fadeInLeft");
+//       this.wrapper.toggle("fadeOutLeft");
+//       this.overlay.remove('fadeIn');
+//       this.overlay.add('fadeOut');
+//       this.mobileMenuCloseButton.remove('on');
+//     }
+//   }
+// }
+// let mobileMenuToggle = new MobileMenu();
